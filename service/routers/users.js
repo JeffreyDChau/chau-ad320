32dda4e0b63a8b246b1b4465a5edd753a12f7cdf
Jeffrey Chau <jeffreyd.chau@gmail.com>
2022-03-17 20:52:48 -0700

    restart

--- service/routers/users.js
+++ service/routers/users.js
@@ -1,18 +1,49 @@
-import { Router } from 'express'
+import { request, Router } from 'express'
+import { body } from 'express-validator'
 import { User } from '../models/User.js'
 
 const usersRouter = Router()
 
+function sanitizeUsers(users) {
+  const sanitizedUsers = users.map((user) => ({
+    id: user._id,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+    decks: user.decks,
+    active: user.active
+  }))
+  return sanitizedUsers
+}
+
 const getUsers = async (req, res) => {
-  const users = await User.find({})
-  res.send(users)
+  const { userId } = req.user
+  const requestor = await User.findById(userId)
+  if (requestor.role === 'admin' || requestor.role === 'superuser') {
+    const users = await User.find({})
+    res.send(sanitizeUsers(users))
+  } else {
+    res.sendStatus(403)
+  }
 }
 
 const getUsersById = async (req, res) => {
-  const user = await User.findById(req.params.id)
-  res.send(user)
+  const { userId } = req.user
+  const requestor = await User.findById(userId)
+  console.log(`[getUsersById] found requestor ${requestor} for userId ${userId} from param ${req.params.id}`)
+  if (requestor.role === 'admin' || 
+    requestor.role === 'superuser' || 
+    requestor._id.toString() === req.params.id.toString()) 
+  {
+    const user = await User.findById(req.params.id)
+    const arr = sanitizeUsers([user])
+    res.send(arr[0])
+  } else {
+    res.sendStatus(403)
+  }
 }
 
+// These routes will remain partially completed for the rest of the course.
 const updateUser = async (req, res) => {
   const result = await User.findByIdAndUpdate(req.params.id, req.body)
   console.log('result ', result)
